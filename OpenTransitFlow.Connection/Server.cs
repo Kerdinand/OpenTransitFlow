4fcb844a40f6ad19ae896b363063d9bf4192bc1a
Kerdinand <ferdinandjonas.kloth@t-online.de>
2025-10-04 12:43:28 +0200

    moving WebSocketServer to different location.

--- OpenTransitFlow.Connection/Server.cs
+++ OpenTransitFlow.Connection/Server.cs
@@ -1,7 +1,32 @@
-﻿namespace OpenTransitFlow.Connection
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace OpenTransitFlow.Connection
 {
+    /// <summary>
+    /// Small wrapper to extract logic outside of Program.s
+    /// </summary>
     public class Server
     {
+        /// <summary>
+        /// Used to capture incoming Stream as of now.
+        /// </summary>
+        TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+
+        /// <summary>
+        /// Basic Function to start a new WebSocketConnection on default ip/port.
+        /// </summary>
+        /// <returns>Returns internal TcpListener</returns>
+        public async Task<TcpListener> StartAndListen()
+        {
+            server.Start();
+            Console.WriteLine("Server Started");
+            await server.AcceptTcpClientAsync();
+            Console.WriteLine($"Client connected from");
+            return server;
+        }
+
+
 
     }
 }
--- OpenTransitFlow.Demo/Program.cs
+++ OpenTransitFlow.Demo/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Net;
-using System.Net.Sockets;
-
+﻿using System.Net.WebSockets;
 
 namespace OpenTransitFlow.Demo
 {
@@ -8,13 +6,7 @@ namespace OpenTransitFlow.Demo
     {
         static void Main(string[] args)
         {
-            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
-            server.Start();
-
-            Console.WriteLine("Server has started on 127.0.0.1:80.{0}Waiting for a connection…", Environment.NewLine);
-
-            TcpClient client = server.AcceptTcpClient();
-
+            
             Console.WriteLine("A client connected.");
         }
     }
