cfb17292d3afe496957f0d78cc319448cfa1c99a
Kerdinand <ferdinandjonas.kloth@t-online.de>
2025-10-24 15:19:18 +0200

    clean up program.cs

--- OpenTransitFlow.Connection/Program.cs
+++ OpenTransitFlow.Connection/Program.cs
@@ -1,6 +1,7 @@
 // Program.cs
 using System.Net.WebSockets;
 using System.Text;
+using OpenTransitFlow.Connection;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -8,41 +9,13 @@ var app = builder.Build();
 builder.WebHost.UseUrls("http://0.0.0.0:5189");
 
 app.UseWebSockets();
-
+Server server = new Server();
 app.Map("/ws", async context =>
 {
-    if (!context.WebSockets.IsWebSocketRequest)
-    {
-        context.Response.StatusCode = 400;
-        return;
-    }
 
-    using var socket = await context.WebSockets.AcceptWebSocketAsync();
-
-    var buffer = new byte[64 * 1024];
-    var sb = new StringBuilder();
-
-    while (true)
-    {
-        var result = await socket.ReceiveAsync(buffer, context.RequestAborted);
-        if (result.MessageType == WebSocketMessageType.Close)
-        {
-            await socket.CloseAsync(WebSocketCloseStatus.NormalClosure, "Closing", context.RequestAborted);
-            break;
-        }
-
-        sb.Append(Encoding.UTF8.GetString(buffer, 0, result.Count));
-
-        if (result.EndOfMessage)
-        {
-            var message = sb.ToString();
-            sb.Clear();
-            Console.WriteLine($"[WS] Received: {message}");
-
-            var ack = Encoding.UTF8.GetBytes("{\"ok\":true}");
-            await socket.SendAsync(ack, WebSocketMessageType.Text, true, context.RequestAborted);
-        }
-    }
+    using var socket = await server.AcceptWebSocketAsync(context);
+    var result = await server.ReadStreamToSyncDebug(socket, context);
+    Console.WriteLine(result);
 });
 
 app.Run();
