97ae6987433ad6695f6fe2eec986626b798f2f9d
Kerdinand <ferdinandjonas.kloth@t-online.de>
2025-10-26 21:59:48 +0100

    Revert "removed old projects and clean up"
    This reverts commit dc0635c714da8b52a4fd1c8752900a71147fceb8.

---
+++ OpenTransitFlow.Infra/Graph/GraphFactory.cs
@@ -0,0 +1,28 @@
+﻿using OpenTransitFlow.Connection;
+using QuikGraph;
+
+namespace OpenTransitFlow.Infra.Graph
+{
+    internal class GraphFactory
+    {
+        public static BidirectionalGraph<NetworkGraphVertex, NetworkGraphEdge> Create(IEnumerable<BaseTrackJson> tracks)
+        {
+            var graph = new BidirectionalGraph<NetworkGraphVertex, NetworkGraphEdge>(true);
+            
+            var nodes = new HashSet<string>();
+            var edges = new HashSet<(string,string)>();
+
+
+            /// TODO: Adding of Edges. Before that DTO and frontend class needs to be updated :/
+            foreach (BaseTrackJson track in tracks) 
+            {
+                nodes.Add(track.inboundDiverging);
+                nodes.Add(track.outboundDiverging);
+                nodes.Add(track.inboundTrack);
+                nodes.Add(track.outboundTrack);
+            }
+
+            return graph;
+        }
+    }
+}
---
+++ OpenTransitFlow.Infra/Graph/NetworkGraphEdge.cs
@@ -0,0 +1,15 @@
+﻿using QuikGraph;
+
+namespace OpenTransitFlow.Infra.Graph
+{
+    internal class NetworkGraphEdge : IEdge<NetworkGraphVertex>
+    {
+        public NetworkGraphEdge()
+        {
+        }
+
+        NetworkGraphVertex IEdge<NetworkGraphVertex>.Source => throw new NotImplementedException();
+
+        NetworkGraphVertex IEdge<NetworkGraphVertex>.Target => throw new NotImplementedException();
+    }
+}
