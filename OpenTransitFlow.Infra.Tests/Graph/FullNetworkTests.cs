aad5e08e2914cb31e81f1f2037329c679d5fac27
Kerdinand <ferdinandjonas.kloth@t-online.de>
2025-11-02 23:16:54 +0100

    added test

---
+++ OpenTransitFlow.Infra.Tests/Graph/FullNetworkTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenTransitFlow.Infra.Simulation;
+
+namespace OpenTransitFlow.Infra.Tests.Graph
+{
+    /// <summary>
+    /// Test class to build time sensetive simulation setup
+    /// </summary>
+    internal class FullNetworkTests
+    {
+        [Test]
+        public void CircleLineBasicTest()
+        {
+            var simulationRunner = new Runner();
+        }
+    }
+}
--- OpenTransitFlow.Infra/Simulation/Runner.cs
+++ OpenTransitFlow.Infra/Simulation/Runner.cs
@@ -3,7 +3,7 @@ namespace OpenTransitFlow.Infra.Simulation
     /// <summary>
     /// Class that runs the simulation. All new objects must be added to it. 
     /// </summary>
-    public static class Runner
+    public class Runner
     {
         
     }
